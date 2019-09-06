1b065b9fd4cf336cacb5152f6a6b259ca750957e
Bjorne1 <35253647+Bjorne1@users.noreply.github.com>
2019-09-06 15:23:34 +0800

    add order validation

--- src/main/java/com/wcs/mall/address/entity/MallAddress.java
+++ src/main/java/com/wcs/mall/address/entity/MallAddress.java
@@ -14,7 +14,7 @@ import javax.validation.constraints.Size;
 @Table(name = "address")
 public class MallAddress extends BaseEntity {
     private String userId;
-    @Size(max = 10, message = "收件人名称为1-10")
+    @Size(max = 10, message = "收件人名称长度为1-10位")
     @NotBlank(message = "收件人名称不能为空")
     private String receiverName;
     @NotBlank(message = "手机号不能为空")
@@ -23,7 +23,7 @@ public class MallAddress extends BaseEntity {
     @NotBlank(message = "省市县不能为空")
     private String region;
     @NotBlank(message = "详细地址不能为空")
-    @Size(message = "详细地址长度为1-50位")
+    @Size(max = 50,message = "详细地址长度为1-50位")
     private String detailAddress;
     private int status;
 
--- src/main/java/com/wcs/mall/order/entity/MallOrderDTO.java
+++ src/main/java/com/wcs/mall/order/entity/MallOrderDTO.java
@@ -2,6 +2,8 @@ package com.wcs.mall.order.entity;
 
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.math.BigDecimal;
 
 /**
@@ -12,10 +14,17 @@ import java.math.BigDecimal;
 @Data
 public class MallOrderDTO {
     private String userId;
+    @NotBlank(message = "商品不能为空")
     private String cartIds;
     private Boolean useCoupon; //是否使用优惠卷
     private BigDecimal coupon;
+    @NotBlank(message = "收件人不能为空")
+    @Size(max = 10, message = "收件人名称长度为1-10位")
     private String receivedName;
+    @NotBlank(message = "手机号不能为空")
+    @Size(max = 20, message = "手机号长度为1-20位")
     private String telephone;
+    @NotBlank(message = "地址不能为空")
+    @Size(max = 50, message = "详细地址长度为1-50位")
     private String address;
 }
